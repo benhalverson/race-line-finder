fc287e7cdd5dc527af2c16e7a7b0ec546e3bc0d3
Ben Halverson <benhalverson33@gmail.com>
2023-05-14 22:19:00 -0700

    save images to frames dir

--- video-to-image.py
+++ video-to-image.py
@@ -1,22 +1,36 @@
+import os
 import cv2
 
-# load the video
-video = cv2.VideoCapture("GX010180.MP4")
+# Directory containing the videos
+video_dir = 'videos'  # replace with your directory path
 
-# get the total number of frames
-frame_number = 0
+# Directory to save the frames
+frame_dir = 'frames'  # replace with your directory path
 
-while True:
-  success, frame = video.read()
+# Get a list of all video files in the video directory
+video_files = [f for f in os.listdir(video_dir) if f.endswith('.MP4')]
 
-  if success:
-    cv2.imwrite(f'frame_{frame_number:04d}.png', frame)
-    frame_number += 1
-  else:
-    # no more frames
-    break
+# Loop over all video files
+for video_file in video_files:
+    # Open the video file
+    video = cv2.VideoCapture(os.path.join(video_dir, video_file))
 
-print("Total frames: ", frame_number)
-video.release()
+    # Initialize frame counter
+    frame_num = 0
 
-print("Done!")
+    while True:
+        # Read the next frame
+        success, frame = video.read()
+
+        # If the frame was read successfully, save it as a PNG image
+        if success:
+            cv2.imwrite(os.path.join(frame_dir, f'{video_file}_{frame_num:04d}.png'), frame)
+            frame_num += 1
+        else:
+            # If no frame could be read, then we have reached the end of the video
+            break
+
+    # Release the video object
+    video.release()
+
+print("Processing of all videos completed.")
