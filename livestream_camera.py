be27b1f2f923a31e5b8aff7fd361c40fee8c9be2
Ben Halverson <benhalverson33@gmail.com>
2023-06-12 15:20:30 -0700

    save livestream to file

--- livestream_camera.py
+++ livestream_camera.py
@@ -1,13 +1,24 @@
 import cv2
 
 capture = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+output = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
 
-while True:
+print(capture.isOpened())
+
+while capture.isOpened():
+  print('Capturing frame')
+  
   ret, frame = capture.read()
-  
-  cv2.imshow('Video', frame)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
+  if ret == True:
+
+    output.write(frame)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+  else:
     break
-  
+    
 capture.release()
+output.release()
 cv2.destroyAllWindows()
